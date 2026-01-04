2fb9f6110e8f14d408c58f5d5c130e85b8de2583
Sahib-Aujla <sahibpreet568@gmail.com>
2026-01-04 17:14:46 -0400

    added wallet clients

---
+++ backend/src/viem.ts
@@ -0,0 +1,32 @@
+import { createWalletClient, createPublicClient, http } from "viem";
+//import { cronosTestnet } from "viem/chains";
+import { privateKeyToAccount } from "viem/accounts";
+import { config } from "./config";
+
+
+export const cronosTestnet = {
+    id: config.chainId,
+    name: "Cronos Testnet",
+    nativeCurrency: {
+        name: "TCRO",
+        symbol: "TCRO",
+        decimals: 18,
+    },
+    rpcUrls: {
+        default: { http: [config.rpc] },
+    },
+};
+
+export const publicClient = createPublicClient({
+    chain: cronosTestnet,
+    transport: http(),
+});
+
+
+const account = privateKeyToAccount(`0x${config.agentKey}`);
+
+export const walletAccount = createWalletClient({
+    chain: cronosTestnet,
+    transport: http(),
+    account: account,
+});
